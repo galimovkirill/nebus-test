5914cd507e56f4b89801423d759b0be73013a618
Кирилл Галимов <galimovdev@gmail.com>
2026-09-02 23:56:11 +0300

    feat: setup eslint, prettier, update nuxt.config

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import withNuxt from './.nuxt/eslint.config.mjs'
+import prettier from 'eslint-config-prettier'
+
+export default withNuxt(
+  {
+    rules: {
+      'vue/html-self-closing': 'off'
+    }
+  },
+  prettier
+)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,40 @@
+const pollingOptions = { usePolling: true, interval: 300 }
+
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-09-01',
+
+  ssr: false,
+
   devtools: { enabled: true },
+
+  watchers: {
+    chokidar: pollingOptions
+  },
+
+  modules: ['@pinia/nuxt', '@nuxt/eslint'],
+
+  components: false,
+
+  imports: {
+    autoImport: false
+  },
+
+  typescript: {
+    strict: true,
+    typeCheck: true
+  },
+
+  app: {
+    head: {
+      htmlAttrs: { lang: 'ru' },
+      title: 'Заметки',
+      meta: [{ charset: 'utf-8' }, { name: 'description', content: 'Заметки со списками задач' }]
+    }
+  },
+
   vite: {
     server: {
-      watch: { usePolling: true, interval: 300 },
-    },
-  },
-});
+      watch: pollingOptions
+    }
+  }
+})
