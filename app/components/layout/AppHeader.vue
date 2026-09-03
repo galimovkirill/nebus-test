c57147ee0d1b8df085f7f19769d733dbb6f24f9a
Кирилл Галимов <galimovdev@gmail.com>
2026-09-03 15:12:55 +0300

    feat: basic layout of note creation form

--- app/components/layout/AppHeader.vue
+++ app/components/layout/AppHeader.vue
@@ -2,12 +2,18 @@
   <header class="app-header">
     <h1 class="app-header__title">Заметки</h1>
 
-    <BaseButton variant="primary">Новая заметка</BaseButton>
+    <BaseButton variant="primary" @click="isNewNoteModalShown = true"> Новая заметка </BaseButton>
   </header>
+
+  <NewNoteModal v-model:open="isNewNoteModalShown" />
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import BaseButton from '~/components/ui/BaseButton.vue'
+import NewNoteModal from '~/features/notes/components/NewNoteModal.vue'
+
+const isNewNoteModalShown = ref(false)
 </script>
 
 <style lang="scss" scoped>
---
+++ app/features/notes/components/NewNoteModal.vue
@@ -0,0 +1,32 @@
+<template>
+  <BaseModal v-model:open="open" class="new-note">
+    <template #title>Создать новую заметку</template>
+
+    <div>
+      <NoteItem text="Помыть посуду" editable />
+    </div>
+
+    <BaseButton variant="text" class="new-note__add">Добавить пункт</BaseButton>
+
+    <template #footer>
+      <BaseButton class="new-note__add" @click="open = false">Закрыть</BaseButton>
+      <BaseButton class="new-note__add" variant="primary">Сохранить</BaseButton>
+    </template>
+  </BaseModal>
+</template>
+
+<script lang="ts" setup>
+import BaseButton from '~/components/ui/BaseButton.vue'
+import BaseModal from '~/components/ui/BaseModal.vue'
+import NoteItem from '~/features/notes/components/NoteItem.vue'
+
+const open = defineModel<boolean>('open', { required: true })
+</script>
+
+<style lang="scss">
+.new-note {
+  &__add {
+    margin-top: 8px;
+  }
+}
+</style>
